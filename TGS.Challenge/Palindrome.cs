9c0da91b806ccbd5175cec826a70d06f50b732a2
Josh Gray <josh.w.gray@gmail.com>
2025-01-11 16:11:01 +0000

    Added Palindrome checker

---
+++ TGS.Challenge/Palindrome.cs
@@ -0,0 +1,26 @@
+namespace TGS.Challenge;
+
+/*
+      Devise a function that checks if a word is a palindrome, if the word is a palindrome return true, else return false
+
+      "Palindrome": a word, phrase, number, or sequence of characters that reads the same backward as forward, ignoring spaces, punctuation, and capitalization. For example
+      Words: radar, level, madam
+      Phrases: A man, a plan, a canal, Panama
+      Numbers: 121, 12321
+
+      isPalindrome("radar") should return true
+      isPalindrome("delightful") should return false
+
+      NOTE: Punctuation, including spaces should be ignored, e.g.
+
+      There are accompanying unit tests for this exercise, ensure all tests pass & make
+      sure the unit tests are correct too.
+   */
+
+public class Palindrome
+{
+    public bool isPalindrome(string word)
+    {
+        return false;
+    }
+}
